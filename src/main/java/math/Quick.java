0457fae4e31e90daf01d87b768e8c2f0f1f150f9
付广中 <fgzhong@fgzhong.local>
2018-09-14 00:40:54 +0800

    test

--- src/main/java/math/Quick.java
+++ src/main/java/math/Quick.java
@@ -3,7 +3,13 @@ package math;
 /**
  * @Author create by fgzhong
  * @Date 2018/9/13
- * @Description
+ * @Description 快速排序
  */
 public class Quick {
+
+    public int[] quickSort(int[] array, int low, int high) {
+        return array;
+    }
+
+
 }
